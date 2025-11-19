0c507e29d09a2e4779f87836ef9191d0030eba9c
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-11-19 21:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+        const form = document.querySelector(".actions");
+        const gridItems = document.querySelectorAll(".grid-item");
+        console.log(gridItems)
+const changeButton = document.getElementById("change_button");
+const reset = document.getElementById("Reset");
+
+
+form.addEventListener("submit", changeColor);
+
+function changeColor(e){
+    e.preventDefault(); 
+    const selectedBox = e.target[0].value;
+    const color = e.target[1].value;
+	console.log(selectedBox, color, "event dta")
+
+
+    const targetBox = document.getElementById(selectedBox);
+    targetBox.style.backgroundColor = color;
+}
+
+    reset.addEventListener("click", ()=>{
+        for(let i of gridItems){
+            // console.log(i);
+            i.style.backgroundColor = "transparent";
+        }
+    })
